0b6387334c4289572c923ce110beaf128b9c2e8b
b1gman1 <98741558+b1gman1@users.noreply.github.com>
2022-10-23 13:54:54 +0600

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,18 @@
+Javascript
+var a;
+function change_color(){
+    if(a==1){
+    document.querySelector('.hii').style.color= 'black';
+    return a=0;}
+    else{
+    document.querySelector('.hii').style.color= 'white';
+    return a=1;}
+    // if(a==1){
+    //     document.getElementById("inter").style.display="inline";
+    //     return a=0;
+    // }
+    // else{
+    //     document.getElementById("inter").style.display="none";
+    //     return a=1;
+    // }
+    }
